68a9484086213a3d1abc759297c03ec4753b54e7
Veto Ramirez <vetorobert3@gmail.com>
2020-03-26 18:50:00 -0700

    Working on axios call.

--- practice/src/components/Feed.js
+++ practice/src/components/Feed.js
@@ -1,7 +1,25 @@
-import react from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const [users, setUsers] = useState(),
+
+useEffect(() => {
+  axios.get('url')
+  .then(res => {
+    console.log(res)
+    setUsers(res)
+  })
+  .catch(err => {
+    console.log(err.message)
+  })
+})
 
 function Feed() {
-  return null;
+  return (
+    <div className="Feed">
+
+    </div>
+  );
 }
 
 export default Feed;
