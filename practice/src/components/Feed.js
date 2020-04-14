c0b7798e362688f9d05632a349bfff4c9a18e41d
Veto Ramirez <vetorobert3@gmail.com>
2020-04-13 21:19:44 -0700

    Pushing all front end stuff.

--- practice/src/components/Feed.js
+++ practice/src/components/Feed.js
@@ -1,23 +1,42 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-const [users, setUsers] = useState(),
-
-useEffect(() => {
-  axios.get('url')
-  .then(res => {
-    console.log(res)
-    setUsers(res)
-  })
-  .catch(err => {
-    console.log(err.message)
-  })
-})
-
 function Feed() {
+
+  const [users, setUsers] = useState([
+    {
+      name: 'Ryan',
+      comment: 'Hello'
+    },
+    {
+      name: 'Jason',
+      comment: 'World'
+    },
+    {
+      name: 'Conary',
+      comment: 'Uhhhhhh'
+    }
+  ]);
+
+  useEffect(() => {
+    axios.get('url')
+    .then(res => {
+      console.log(res)
+      setUsers(res)
+    })
+    .catch(err => {
+      console.log(err.message);
+    })
+  }, [])
+
   return (
     <div className="Feed">
-
+      {users.map(user => (
+        <div>
+          <div>user.name</div>
+          <div>user.commit</div>
+        </div>
+      ))}
     </div>
   );
 }
--- practice/src/components/Form.js
+++ practice/src/components/Form.js
@@ -1,7 +1,43 @@
-import react from 'react';
+import React, { useState } from 'react';
+// import Axios from 'axios';
 
 function Form() {
-  return null;
+
+  const [post, setPost] = useState({
+    name: '',
+    comment: ''
+  });
+
+  function changeHandler(event) {
+    console.log(post)
+    setPost({...post, [event.target.name]: event.target.value})
+  }
+
+  // function submitHandler() {
+  //   Axios.post('url', post)
+  // }
+
+  return (
+    <div className='form'>
+      <form>
+        <input 
+          type='text' 
+          name='name' 
+          placeholder='Name' 
+          value={post.name} 
+          onChange={changeHandler}
+        />
+        <input 
+          type='text' 
+          name='comment' 
+          placeholder='Comment' 
+          value={post.comment}
+          onChange={changeHandler}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  )
 }
 
 export default Form;
